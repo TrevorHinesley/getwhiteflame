08fb255fae9cd0e251a376620a5f122735db1dda
Trevor Hinesley <trevor.hinesley@gmail.com>
2013-11-14 17:20:34 -0600

    Pushing files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HHSoftwareSolutions::Application.config.secret_key_base = '68087a9985b08f0c7e69dbcbb57087252fbf795a04d9bc2cf9ed176d2337a5555e3de8f3834271c986f5c19817d1ef1a7def2d9c63f3756cdc8e85bcfba2b39d'
