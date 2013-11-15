8876d7d76973b7a9d1a4e7ef28d30ef3488a2d70
Trevor Hinesley <trevor.hinesley@gmail.com>
2013-11-14 22:38:45 -0600

    working on getting files to download still

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,10 @@ class WelcomeController < ApplicationController
 	end
 
   def download_song
-    send_file "#{Rails.root}/public/white-flame.wav", :x_sendfile => true, :type=>"audio/wav", :filename => "white-flame.wav"
+    send_file "#{Rails.root}/public/white-flame.wav", :type=>"application/octet-stream", :filename => "white-flame.wav"
   end
 
   def download_artwork
-    send_file "#{Rails.root}/public/white-flame-artwork.jpg", :x_sendfile => true, :type=>"image/jpg", :filename => "white-flame-artwork.jpg"
+    send_file "#{Rails.root}/public/white-flame-artwork.jpg", :type=>"application/octet-stream", :filename => "white-flame-artwork.jpg"
   end
 end
