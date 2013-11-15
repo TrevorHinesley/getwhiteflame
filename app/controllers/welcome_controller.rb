14d6c2e072193bb94eef3524268d5db449520520
Trevor Hinesley <trevor.hinesley@gmail.com>
2013-11-14 19:12:50 -0600

    working on speeding up file download

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,10 @@ class WelcomeController < ApplicationController
 	end
 
   def download_song
-    send_file "#{Rails.root}/app/assets/media/white-flame.wav", :type=>"audio/wav", :filename => "white-flame.wav"
+    send_file "#{Rails.root}/app/assets/media/white-flame.wav", :x_sendfile => true, :type=>"audio/wav", :filename => "white-flame.wav"
   end
 
   def download_artwork
-    send_file "#{Rails.root}/app/assets/images/white-flame-artwork.jpg", :type=>"image/jpg", :filename => "white-flame.jpg"
+    send_file "#{Rails.root}/app/assets/images/white-flame-artwork.jpg", :x_sendfile => true, :type=>"image/jpg", :filename => "white-flame.jpg"
   end
 end
