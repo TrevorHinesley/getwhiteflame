25750d9a964422cbc94ce002933c17e99427af5c
Trevor Hinesley <trevor.hinesley@gmail.com>
2013-11-18 17:15:56 -0600

    Added xmas video

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,14 @@ class WelcomeController < ApplicationController
 	def index
 	end
 
+  def merry_christmas
+  end
+
   def download_song
-    send_file "#{Rails.root}/public/white-flame.wav", :type=>"application/octet-stream", :filename => "white-flame.wav"
+    send_file "#{Rails.root}/public/white-flame.wav"
   end
 
   def download_artwork
-    send_file "#{Rails.root}/public/white-flame-artwork.jpg", :type=>"application/octet-stream", :filename => "white-flame-artwork.jpg"
+    send_file "#{Rails.root}/public/white-flame-artwork.jpg"
   end
 end
